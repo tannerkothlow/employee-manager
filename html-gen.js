6f24f2027c3f12db3ab3230e07ce60ea6e0c1ea3
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-03 12:13:17 -0500

    Files established according to Guidelines

---
+++ html-gen.js
@@ -0,0 +1,3 @@
+// This will be what the Inquirer data is passed into and assembled.
+
+// Each card object will be built on a boilerplate object that will be extended depending on the criteria entered.
