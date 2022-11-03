d5183f4992dde638ee7f3d99b0cdb8626fdb8284
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-03 13:24:17 -0500

    Manager questions established

--- index.js
+++ index.js
@@ -2,17 +2,33 @@
 const inquirer = require('inquirer');
 const htmlGen = require('./html-gen');
 
+const Manager = require('./classes/Manager');
 
+// Manager Prompt
 init = () => {
     inquirer
         .prompt([
             {
-                message: `This is a test question`,
-                name: `testResponse`
+                message: `Enter the team manager's name:`,
+                name: `managerName`
+            },
+            {
+                message: `Enter the team manager's employee ID:`,
+                name: `managerId`
+            },
+            {
+                message: `Enter the team manager's email address:`,
+                name: `managerEmail`
+            },
+            {
+                message: `Enter the team manager's office number:`,
+                name: `managerOffice`
             }
         ])
         .then((response) => {
-            console.log(response.testResponse);
+            const {managerName, managerId, managerEmail, managerOffice} = response;
+            const manager = new Manager(managerName, managerId, managerEmail, managerOffice);
+            console.log(manager);
         })
 }
 
