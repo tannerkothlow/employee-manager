8a84654fd022c5cc9aa6345e0784a0c7393a36f7
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-03 13:14:14 -0500

    Established Inquirer prompt

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Main Inquirer prompt
+const inquirer = require('inquirer');
+const htmlGen = require('./html-gen');
+
+
+init = () => {
+    inquirer
+        .prompt([
+            {
+                message: `This is a test question`,
+                name: `testResponse`
+            }
+        ])
+        .then((response) => {
+            console.log(response.testResponse);
+        })
+}
+
+init()
