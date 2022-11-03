71a3b99afec1e2aa62ebc6fa62ede5df8b2b2b58
Tanner Kothlow <tannerkothlow@msn.com>
2022-11-03 15:02:49 -0500

    Card maker function tested and functional

--- test/html-gen.test.js
+++ test/html-gen.test.js
@@ -1,5 +1,7 @@
 const GenTest = require('../html-gen');
 const Manager = require('../classes/Manager');
+const Engineer = require('../classes/Engineer');
+const Intern = require('../classes/Intern')
 
 // TO RUN: Uncomment the GenTest class and module exports for GenTest in html-gen.js
 describe('htmlGen', () => {
@@ -12,10 +14,22 @@ describe('htmlGen', () => {
 
         expect(obj.makeCard(team)).toEqual('Manager emp card');
     })
-    xit('should return and engineer card if getRole returns Engineer', () => {
+    it('should return and engineer card if getRole returns Engineer', () => {
+        const team = [];
+        const dummy = new Engineer('Greg', 100, 'greg@email.com', 'greghub');
+        team.push(dummy);
 
+        const obj = new GenTest;
+
+        expect(obj.makeCard(team)).toEqual('Engineer emp card');
     })
-    xit('should return an intern card if getRole returns Intern', () => {
+    it('should return an intern card if getRole returns Intern', () => {
+        const team = [];
+        const dummy = new Intern('Greg', 100, 'greg@email.com', 'Gregcademy');
+        team.push(dummy);
 
+        const obj = new GenTest;
+
+        expect(obj.makeCard(team)).toEqual('Intern emp card');
     })
 })
